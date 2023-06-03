b42eb11b2efe32ab872f4ae4cd5ef317583fe83b
ejercicios-1 <kiritooo956@gmail.com>
2023-06-03 09:31:42 -0500

    subir el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+const  App = () => <div className="contenedor">
+    <div className="titulo">
+        
+        <h1>hello</h1>
+    </div>
+</div>;
+export default App;
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+import './estilos/index.css';
+import App from './App';
+
+const container = document.getElementById('root');
+
+createRoot(container).render(<App />);
